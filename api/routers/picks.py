7113f65ea007f359aa76c08be20a20742abd2c1e
John Murray <johnmurray@fastmail.com>
2023-03-06 08:48:25 -0500

    adds utility functinos

--- api/routers/picks.py
+++ api/routers/picks.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
+
+from utility.combinations import get_combinations
+from utility.responses import init_response
 from utility.strings import generate_url, is_hex_color
 
 prefix = "/picks"
@@ -11,7 +14,7 @@ router = APIRouter(
 
 
 @router.get("/pair")
-def get_results(color_one: str = "#000000", color_two: str = "#FFFFFF"):
+def get_scores(color_one: str = "#000000", color_two: str = "#FFFFFF"):
     """Get accessibility results for a pair of hex codes."""
 
     import requests
@@ -26,3 +29,22 @@ def get_results(color_one: str = "#000000", color_two: str = "#FFFFFF"):
 
     else:
         return {"message": "Please input a valid hexadecimal color value."}
+
+
+@router.post("/combos")
+def get_combos(colors: list[str]):
+
+    if all([is_hex_color(color) for color in colors]):
+        response = init_response()
+
+        combos = get_combinations(colors, 2)
+        for combo in combos:
+            color_one = combo[0]
+            color_two = combo[1]
+            colorCombo = {"colorOne": color_one, "colorTwo": color_two}
+            scores = get_scores(color_one, color_two)
+            colorCombo["results"] = scores
+            response["colorCombos"].append(colorCombo)
+        return {"message": "WebAIM API request successful.", "results": response}
+    else:
+        return {"message": "1 or more colors is invalid."}
---
+++ api/utility/responses.py
@@ -0,0 +1,9 @@
+def init_response() -> dict:
+    from utility.dates import get_epoch, get_utc_datetime
+
+    """Initialize an response object"""
+    response = {}
+    response["requestId"] = str(get_epoch())
+    response["requestTs"] = get_utc_datetime().strftime("%Y-%m-%d %H:%M:%S")
+    response["colorCombos"] = []
+    return response
